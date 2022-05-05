38b6dce3182f4e9e452516374ab455607a83983c
Tanvir Ahmed <mail2t.ahmed@gmail.com>
2022-05-05 09:09:01 +0900

    initial commit for the _trace struct

--- include/Trace.hpp
+++ include/Trace.hpp
@@ -5,7 +5,14 @@
 #include <cstdint>
 
 struct Trace {
-    struct _trace {};
+    struct _trace {
+      unsigned int program_counter;
+      unsigned int instruction;
+      unsigned int instruction_type; //0: nop, 1: arith, 2: logic, 3: jump
+                                      //4: branch, 5: load, 6: store
+      unsigned int memory_address; //if memory instruction (load and store)
+      unsigned int branch_target; //if jump and branch instruction
+    };
     std::vector<_trace> trace;
     void generate(unsigned char *memory, unsigned program_counter);
     uint64_t reg_file[32];
